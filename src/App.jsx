3be799820ffb2489b74584a40c1dbc70d1ac9cd6
rattikarn <rattikarnpharakarn@gmail.com>
2024-03-24 20:37:28 +0700

    del comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import {
   BrowserRouter,
-  Routes, // instead of "Switch"
+  Routes, 
   Route,
 } from "react-router-dom";
 
 import Home from "./pages/home";
 import "./App.css";
-//redux
+
 import { Provider } from "react-redux";
-// import { legacy_createStore as createStore } from "redux";
-// import rootReducer from "./redux/reducers/index";
-// const store = createStore (rootReducer);
 import { createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
 import rootReducer from "./store/reducers/index";
 import LoadingComponent from "./components/LoadingComponent";
 import Layout from "./pages/layout/index";
 
-//redux persists
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
@@ -28,7 +24,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          {/* Add other routes here */}
         </Route>
       </Routes>
       <LoadingComponent />
